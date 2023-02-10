c26fc29544c2bb0e39c367f9a89c11d750177951
Karina Hristova <52160729+karinaghristova@users.noreply.github.com>
2023-02-10 20:07:53 +0200

    Added models and changed fields of existing models

--- schule/accounts/models.py
+++ schule/accounts/models.py
@@ -1,49 +1,78 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 from datetime import date
 
+
+class City(models.Model):
+    name = models.CharField(max_length=200, blank=False, unique=True)
+    
+    def __str__(self):
+        return self.name
+
 # Create your models here.
+class School(models.Model):
+    name = models.CharField(max_length=200, blank=False)
+    city = models.ForeignKey(City, blank=False, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
+
 class Teacher(models.Model):
     user = models.OneToOneField(
         User, null=False, blank=False, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=200, blank=False)
-    last_name = models.CharField(max_length=200, blank=False)
+    first_name = models.CharField(max_length=200, default="No name", blank=False)
+    last_name = models.CharField(max_length=200, default="No name", blank=False)
     phone = models.CharField(max_length=200, null=True, blank=True)
     email = models.EmailField(max_length=200, unique=True, blank=False)
+    school = models.ForeignKey(School, blank=False, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'Mr/Mrs {self.first_name} {self.last_name}'
 
 class Student(models.Model):
     user = models.OneToOneField(User, null=False, blank=False, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=200, blank=False)
-    middle_name = models.CharField(max_length=200, null=True, blank=True)
-    last_name = models.CharField(max_length=200, blank=False)
+    first_name = models.CharField(max_length=200, default="No name", blank=False)
+    middle_name = models.CharField(max_length=200, default="No name", null=True, blank=True)
+    last_name = models.CharField(max_length=200, default="No name", blank=False)
     phone = models.CharField(max_length=200, null=True, blank=True)
     email = models.EmailField(max_length=200, unique=True, blank=False)
+    grade_average = models.DecimalField(max_digits=3, default=2.00, decimal_places=2, blank=False)
+    school = models.ForeignKey(School, blank=False, null=True, on_delete=models.SET_NULL)
+    class_level = models.IntegerField(blank=False, default=1, validators=[MinValueValidator(1), MaxValueValidator(12)])
+    student_number = models.IntegerField(blank=False, default=1, validators=[MinValueValidator(1), MaxValueValidator(30)])
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
-
+        return f'{self.student_number}: {self.first_name} {self.last_name}'
 
 class Parent(models.Model):
     user = models.OneToOneField(User, null=False, blank=False, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=200, blank=False)
-    last_name = models.CharField(max_length=200, blank=False)
+    first_name = models.CharField(max_length=200, default="No name", blank=False)
+    last_name = models.CharField(max_length=200, default="No name", blank=False)
     phone = models.CharField(max_length=200, null=True, blank=True)
     email = models.EmailField(max_length=200, unique=True, blank=False)
     child = models.ForeignKey(Student, null=True, on_delete=models.SET_NULL)
+    school = models.ForeignKey(School, blank=False, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
 
 
 class Subject(models.Model):
-    name = models.CharField(max_length=200, blank=False)
+    name = models.CharField(max_length=200, blank=False, unique=True)
 
     def __str__(self):
         return self.name
+
+class SubjectClass(models.Model):
+    class_level = models.IntegerField(blank=False, default=1, validators=[MinValueValidator(1), MaxValueValidator(12)])
+    subject = models.ForeignKey(Subject, null=True, on_delete=models.CASCADE, blank=False)
+    teacher = models.ForeignKey(Teacher, null=True, on_delete=models.CASCADE, blank=False)
+    students = models.ManyToManyField(Student, blank=True)
+
+    def __str__(self):
+        return f'{self.subject} class of Mr/Mrs {self.teacher.last_name}'
 
 class Term(models.Model):
     CHOICES = (('winter', 'winter'), ('summer', 'summer'))
@@ -54,21 +83,21 @@ class Term(models.Model):
 
 
 class Grade(models.Model):
-    number = models.DecimalField(max_digits=3, decimal_places=2, blank=False)
+    CHOICES = ((2, 'Failure 2'), (3, 'Passing 3'), (4, 'Good 4'), (5, 'Very good 5'), (6, 'Excellent 6'))
+    number = models.IntegerField(blank=False, choices=CHOICES)
     student = models.ForeignKey(Student, null=True, on_delete=models.CASCADE, blank=False)
-    teacher = models.ForeignKey(Teacher, null=True, on_delete=models.CASCADE, blank=False)
+    subject_class = models.ForeignKey(SubjectClass, null=True, on_delete=models.SET_NULL, blank=False)
     term = models.ForeignKey(Term, null=True, blank=False, on_delete=models.SET_NULL)
 
     def __str__(self):
-        return self.number
+        return f'{self.number}'
 
 
 class Absence(models.Model):
     date = models.DateField(default=date.today, blank=False)
     excused = models.BooleanField(default=False, blank=False)
-    subject = models.ForeignKey(Subject, null=True, on_delete=models.CASCADE, blank=False)
+    subject_class = models.ForeignKey(SubjectClass, null=True, on_delete=models.CASCADE, blank=False)
     student = models.ForeignKey(Student, null=True, on_delete=models.CASCADE, blank=False)
-    teacher = models.ForeignKey(Teacher, null=True, on_delete=models.CASCADE, blank=False)
     term = models.ForeignKey(Term, null=True, blank=False, on_delete=models.SET_NULL)
 
     def __str__(self):
@@ -95,12 +124,6 @@ class Praise(models.Model):
         return self.content
 
 
-class SubjectClass(models.Model):
-    subject = models.ForeignKey(Subject, null=True, on_delete=models.CASCADE, blank=False)
-    teacher = models.ForeignKey(Teacher, null=True, on_delete=models.CASCADE, blank=False)
-    students = models.ManyToManyField(Student)
 
-    def __str__(self):
-        return f'{self.subject} class of Mr/Mrs {self.teacher.last_name}'
 
 
