c2835fa453a0e0c5883e5d4cdbfcca0f687f03b0
Karina Hristova <52160729+karinaghristova@users.noreply.github.com>
2023-02-17 05:07:23 +0200

    Added objects in setUp

--- schule/accounts/test/test_views.py
+++ schule/accounts/test/test_views.py
@@ -39,47 +39,86 @@ class TestHomeView(TestCase):
     def setUp(self):
         self.client = Client()
 
-        self.teacher = User.objects.create_user(
-            username='teacher', password='password')
-        self.parent = User.objects.create_user(
-            username='parent', password='password')
-        self.student = User.objects.create_user(
-            username='student', password='password')
-        self.admin = User.objects.create_user(
-            username='admin', password='password')
+        self.city = City.objects.create(name='City')
+        self.school = School.objects.create(name='TestSchool', city=self.city)
+
+        self.teacher_user = User.objects.create_user(
+            username='teacher', password='password', 
+            first_name='Teacher', last_name='Teacher', 
+            email='teacher@email.com')
+        self.teacher = Teacher.objects.create(user=self.teacher_user,
+        first_name=self.teacher_user.first_name, last_name=self.teacher_user.last_name, 
+        email=self.teacher_user.email, school=self.school)
+        
+        self.student_user = User.objects.create_user(
+            username='student', password='password',
+            first_name='Student', last_name='Student', 
+            email='student@email.com')
+        self.student = Student.objects.create(user=self.student_user,
+        first_name=self.student_user.first_name, last_name=self.student_user.last_name, 
+        email=self.student_user.email, school=self.school,
+        class_level=10, student_number=1)
+
+        self.parent_user = User.objects.create_user(
+            username='parent', password='password',
+            first_name='Parent', last_name='Parent', email='parent@email.com')
+        self.parent = Parent.objects.create(user=self.parent_user,
+        first_name=self.parent_user.first_name, last_name=self.parent_user.last_name, 
+        email=self.parent_user.email, school=self.school)
+
+        self.admin_user = User.objects.create_superuser(
+            username='admin', password='password',
+            first_name='Admin', last_name='Admin', email='admin@email.com')
 
         self.admin_group = Group.objects.create(name='admin')
         self.teacher_group = Group.objects.create(name='teacher')
         self.parent_group = Group.objects.create(name='parent')
         self.student_group = Group.objects.create(name='student')
 
-        self.admin.groups.add(self.admin_group)
-        self.teacher.groups.add(self.teacher_group)
-        self.parent.groups.add(self.parent_group)
-        self.student.groups.add(self.student_group)
+        self.admin_user.groups.add(self.admin_group)
+        self.teacher_user.groups.add(self.teacher_group)
+        self.parent_user.groups.add(self.parent_group)
+        self.student_user.groups.add(self.student_group)
 
-    def tes_home_view_is_accessible_for_users_with_group_admin(self):
+        self.subject = Subject.objects.create(name='Subject')
+        self.subject_class = SubjectClass.objects.create(class_level=10, 
+        subject=self.subject, teacher=self.teacher)
+        self.subject_class.students.add(self.student)
+
+        self.winter_term = Term.objects.create(name='winter')
+        self.summer_term = Term.objects.create(name='summer')
+
+        self.winter_grade = Grade.objects.create(number=4, 
+        student=self.student, subject_class=self.subject_class, 
+        term=self.winter_term)
+
+        self.summer_grade = Grade.objects.create(number=4, 
+        student=self.student, subject_class=self.subject_class, 
+        term=self.summer_term)
+
+
+    def test_home_view_is_accessible_for_staff_users(self):
         self.client.login(username='admin', password='password')
         response = self.client.get(reverse('home'))
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed('accounts/homepage_admin.html')
 
-    def tes_home_view_is_accessible_for_users_with_group_teacher(self):
+    def test_home_view_is_accessible_for_users_with_group_teacher(self):
         self.client.login(username='teacher', password='password')
         response = self.client.get(reverse('home'))
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed('accounts/homepage_teacher.html')
 
-    def tes_home_view_is_accessible_for_users_with_group_parent(self):
+    def test_home_view_is_accessible_for_users_with_group_parent(self):
         self.client.login(username='parent', password='password')
         response = self.client.get(reverse('home'))
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed('accounts/homepage_parent.html')
 
-    def tes_home_view_is_accessible_for_users_with_group_student(self):
+    def test_home_view_is_accessible_for_users_with_group_student(self):
         self.client.login(username='student', password='password')
         response = self.client.get(reverse('home'))
 
