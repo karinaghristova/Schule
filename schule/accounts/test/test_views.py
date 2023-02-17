7f94e4318a51b16b74d7b0af81cdc426b91bae1f
Karina Hristova <52160729+karinaghristova@users.noreply.github.com>
2023-02-17 03:45:07 +0200

    Added tests for home view

--- schule/accounts/test/test_views.py
+++ schule/accounts/test/test_views.py
@@ -35,6 +35,58 @@ class TestHomeViewWithRequiredLogin(TestCase):
         self.assertEquals(response.status_code, 302)
 
 
+class TestHomeView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+        self.teacher = User.objects.create_user(
+            username='teacher', password='password')
+        self.parent = User.objects.create_user(
+            username='parent', password='password')
+        self.student = User.objects.create_user(
+            username='student', password='password')
+        self.admin = User.objects.create_user(
+            username='admin', password='password')
+
+        self.admin_group = Group.objects.create(name='admin')
+        self.teacher_group = Group.objects.create(name='teacher')
+        self.parent_group = Group.objects.create(name='parent')
+        self.student_group = Group.objects.create(name='student')
+
+        self.admin.groups.add(self.admin_group)
+        self.teacher.groups.add(self.teacher_group)
+        self.parent.groups.add(self.parent_group)
+        self.student.groups.add(self.student_group)
+
+    def tes_home_view_is_accessible_for_users_with_group_admin(self):
+        self.client.login(username='admin', password='password')
+        response = self.client.get(reverse('home'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('accounts/homepage_admin.html')
+
+    def tes_home_view_is_accessible_for_users_with_group_teacher(self):
+        self.client.login(username='teacher', password='password')
+        response = self.client.get(reverse('home'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('accounts/homepage_teacher.html')
+
+    def tes_home_view_is_accessible_for_users_with_group_parent(self):
+        self.client.login(username='parent', password='password')
+        response = self.client.get(reverse('home'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('accounts/homepage_parent.html')
+
+    def tes_home_view_is_accessible_for_users_with_group_student(self):
+        self.client.login(username='student', password='password')
+        response = self.client.get(reverse('home'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed('accounts/homepage_student.html')
+
+
 class TestTeacherViewsWithRequiredLogin(TestCase):
     def test_teacher_account_view_redirects_for_unathenticated_users_GET(self):
         response = self.client.get(reverse('teacher_account'))
